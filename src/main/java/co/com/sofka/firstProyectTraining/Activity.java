2d25811e372486989afb107fae85dd0212a22fd1
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-04-12 19:00:27 -0500

    Se actualizan los nombres de las variables

--- src/main/java/co/com/sofka/firstProyectTraining/Activity.java
+++ src/main/java/co/com/sofka/firstProyectTraining/Activity.java
@@ -7,16 +7,16 @@ public class Activity {
         Scanner answerPerson = new Scanner(System.in);
 
         System.out.println("Ingrese por favor su nombre.");
-        String answer1 = answerPerson.nextLine();
+        String name = answerPerson.nextLine();
 
         System.out.println("Ingrese por favor su numero de celular.");
-        String answer2 = answerPerson.nextLine();
+        String phone = answerPerson.nextLine();
 
         System.out.println("Ingrese por favor su edad.");
-        Integer answer3 = answerPerson.nextInt();
+        Integer age = answerPerson.nextInt();
 
-        System.out.println("Bienvenido señor "+ answer1 +", es un placer para nosotros contar con una persona de " +answer3+ " años. \n"
-                            + "Próximamente nos comunicaremos con usted al numero " +answer2+ ". \n"
+        System.out.println("Bienvenido señor "+ name +", es un placer para nosotros contar con una persona de " +age+ " años. \n"
+                            + "Próximamente nos comunicaremos con usted al numero " +phone+ ". \n"
                             + "Feliz día.");
     }
 }
